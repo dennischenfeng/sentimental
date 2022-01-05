b216077e080219f4e13794dcaec7c6dea5d44032
Dennis Feng <dennisfeng2@gmail.com>
2022-01-05 15:01:00 -0800

    working code to have javascript frontend communicate with python backend (with help from Nate Rush). Needs some cleanup though

--- sentimental/app.py
+++ sentimental/app.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, jsonify, request
+import json
 
 app = Flask(__name__)
 
 # Display your index page
 @app.route("/")
 def index():
+    # return "console.log('hi')"
     return render_template('index.html')
 
-# A function to add two numbers
-@app.route("/add")
-def add():
-    a = request.args.get('a')
-    b = request.args.get('b')
-    return jsonify({"result": a+b})
+# @app.route("/result")
+# def result():
+#     input_text = request.args.get('input-text')
+#     # b = request.args.get('b')
+#     return jsonify({"result": input_text + "test"})
+
+@app.route("/test", methods = ['GET', 'POST'])
+def test():
+    print(request.json)
+    a = request.json['a']
+    b = request.json['b']
+    return json.dumps({"sum": a + b})
+
+
+    # return '{"a": "hello world"}'
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
