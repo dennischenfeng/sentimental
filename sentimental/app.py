4052df9365b40f3711b89bc43786a11521791c87
Dennis Feng <dennisfeng2@gmail.com>
2022-01-06 17:01:23 -0800

    now uses pre-trained nltk model

--- sentimental/app.py
+++ sentimental/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 
@@ -9,8 +11,13 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     input_text = request.json['inputText']
-    result = 1 if len(input_text) > 1 else 0
-    return jsonify({'result': result})
+    sid = SentimentIntensityAnalyzer()
+    sentiment = sid.polarity_scores(input_text)["compound"]
+    return jsonify({'result': sentiment})
+    
+    # input_text = request.json['inputText']
+    # result = 1 if len(input_text) > 1 else 0
+    # return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
